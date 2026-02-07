c4ed4d551be1ca6f5920c7cb3bac88f950762e26
Prerit-Bhagat <preritbhagat001@gmail.com>
2026-02-08 00:40:53 +0530

    file client Updated

---
+++ file-service/src/main/java/com/fileservice/dto/FileMetadataDto.java
@@ -0,0 +1,51 @@
+package com.fileservice.dto;
+
+public class FileMetadataDto {
+
+    private String fileId;
+    private String originalName;
+    private String storedName;
+    private Long size;
+    private String contentType;
+
+    // Getters & Setters
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public void setOriginalName(String originalName) {
+        this.originalName = originalName;
+    }
+
+    public String getStoredName() {
+        return storedName;
+    }
+
+    public void setStoredName(String storedName) {
+        this.storedName = storedName;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
